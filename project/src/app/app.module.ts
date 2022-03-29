b0b6b295f6852aa3ea74fc40659f783d71b57cc3
Onur Kiroglu <onur.kiroglu@vendrops.com>
2022-03-30 00:44:58 +0300

    components created

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
