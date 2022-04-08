8c29bf823613bab603a04fa4efa08578fbaf773c
Onur Kiroglu <onur.kiroglu@vendrops.com>
2022-04-08 11:29:36 +0300

    router added

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {path:'',redirectTo:'products',pathMatch:'full'},
+  {path:'products', component:ProductsComponent},
+  {path:'cart', component:CartComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -15,6 +16,7 @@ import {HttpClientModule} from '@angular/common/http';
     ProductsComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
